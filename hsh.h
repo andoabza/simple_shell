836cf4a52deaf55e3afea5d528bfe6ee214ac33f
Adane Moges <adanemogs6@gmail.com>
2023-05-24 13:42:40 +0300

    creating main components

---
+++ hsh.h
@@ -0,0 +1,13 @@
+#ifndef HSH_H
+#define HSH_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+
+#endif
