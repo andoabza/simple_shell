72bd7c00a9bbbb5aec0aa47ce3fe883fab0cb077
andaabza <terbu102@gmail.com>
2023-05-26 22:34:43 -0700

    parser

--- parser.c
+++ parser.c
@@ -7,28 +7,28 @@
  */
 char **parse(char *str)
 {
-	char **parsed;
-	char *token;
-	unsigned int i;
+  char **parsed;
+  char *token;
+  unsigned int i;
 
-	parsed = malloc(sizeof(char) * BUFFER);
-	if (parsed == NULL)
-	{
-		errors(ERR_MALLOC);
-		exit(EXIT_FAILURE);
-	}
+  parsed = malloc(sizeof(char) * BUFFER);
+  if (parsed == NULL)
+    {
+      errors(ERR_MALLOC);
+      exit(EXIT_FAILURE);
+    }
 
-	token = strtok(str, "\n ");
+  token = strtok(str, "\n ");
+  
+  i = 0;
+  while (token != NULL)
+    {
+      parsed[i] = token;
+      token = strtok(NULL, "\n ");
+      i++;
+    }
 
-	i = 0;
-	while (token != NULL)
-	{
-		parsed[i] = token;
-		token = strtok(NULL, "\n ");
-		i++;
-	}
+  parsed[i] = NULL;
 
-	parsed[i] = NULL;
-
-	return (parsed);
+  return (parsed);
 }
